795f3031e799281dd7ccb818e9d05f744f0466da
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-06-10 11:23:22 +0200

    Add files via upload

---
+++ BirdsFlyingAroundAppOpgave/BirdsFlyingAroundAppOpgave/Ostrich.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdsFlyingAroundAppOpgave
+{
+    //Class for an ostrich
+    public class Ostrich : Bird
+    {
+        public override double Speed { get; set; }
+        public override string Color { get; set; }
+
+        public Ostrich(double speed) :base(speed)
+        {
+            this.Speed = speed;
+            this.Color = "An ostrich is black with white wings, legs and neck";
+        }
+
+        public override string Sing()
+        {
+            return $"Ostrich tries to sing but makes a weird noise";
+        }
+
+        public override string Eat()
+        {
+            return "Ostrich eats some food";
+        }
+    }
+}
